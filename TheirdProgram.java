3c2f901cebf98e44a95df25dc823f7c9dec9a482
Ramajayam-S <110026810+Ramajayam-S@users.noreply.github.com>
2022-07-28 15:27:34 +0530

    Add files via upload

---
+++ TheirdProgram.java
@@ -0,0 +1,28 @@
+class TheirdProgram
+{
+	public static void main(String[]args)
+	{
+		
+		System.out.println("Theird program");
+		/*System.out.println(10);
+		System.out.println(20);
+		System.out.println(30);
+		System.out.println(true);
+		System.out.println("End Program");
+		
+		*/		
+		System.out.println("java ++ -- ** &&" + "program" );
+		System.out.println(10 + 30);   //40
+		System.out.println(10 + 30+100);//140
+		System.out.println(10 + 30+" java"); //40java
+		System.out.println("10" + "30"); //1030
+		System.out.println("End Program = " + 10 );
+		/*System.out.println("program ");
+		System.out.println("java +" + "program 1 ");
+		System.out.println("10 " +"20 ");
+		System.out.println("End Program");*/
+		
+		
+		
+	}
+}
